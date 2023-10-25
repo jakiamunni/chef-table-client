cb647594e0f476a1ac9edc4b330d128649506b3a
jakiamunni <jakiasmunni205@gmail.com>
2023-10-25 08:51:32 +0600

    blog and contact updated

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from "react-dom";
-import Pdf from "react-to-pdf";
+// import Pdf from "react-to-pdf";
 import { FaDownload, FaLongArrowAltRight } from 'react-icons/fa';
 const ref = React.createRef();
 
@@ -8,13 +8,46 @@ const Blog = () => {
 
     return (
         <div className='px-20 my-10'>
-            <Pdf targetRef={ref} filename="blog.pdf">
+            {/* <Pdf targetRef={ref} filename="blog.pdf">
                 {({ toPdf }) => <button onClick={toPdf}><FaDownload className=' text-orange-500 text-2xl mb-5'></FaDownload></button>}
-            </Pdf>
+            </Pdf> */}
 
 
             <div ref={ref}>
-                <h2 className='text-xl font-bold'> 1. Tell us the differences between uncontrolled and controlled components. </h2>
+
+            <div className="container mx-auto mt-8">
+        <h1 class="text-3xl font-semibold mb-4">Welcome to Our Blog</h1>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {/* <!-- Blog Post 1 --> */}
+            <div className="bg-white rounded-lg overflow-hidden shadow-md">
+                {/* <img src="https://via.placeholder.com/400x250" alt="Blog Post 1" class="w-full h-40 object-cover"> */}
+                <div className="p-6">
+                    <h2 className="text-xl font-semibold mb-2">Delicious Cuisine</h2>
+                    <p className="text-gray-700">Explore our mouthwatering dishes, carefully prepared by our chefs. </p>
+                    <a href="#" class="text-blue-600 hover:underline">Read More</a>
+                </div>
+            </div>
+            <div className="bg-white rounded-lg overflow-hidden shadow-md">
+                {/* <img src="https://via.placeholder.com/400x250" alt="Blog Post 2" class="w-full h-40 object-cover"> */}
+                <div className="p-6">
+                    <h2 className="text-xl font-semibold mb-2">Chef's Specials</h2>
+                    <p className="text-gray-700">Discover the chef's signature dishes that will tantalize your taste buds.</p>
+                    <a href="#" className="text-blue-600 hover:underline">Read More</a>
+                </div>
+            </div>
+            {/* <!-- Blog Post 3 --> */}
+            <div className="bg-white rounded-lg overflow-hidden shadow-md">
+                {/* <img src="https://via.placeholder.com/400x250" alt="Blog Post 3" className="w-full h-40 object-cover"> */}
+                <div className="p-6">
+                    <h2 className="text-xl font-semibold mb-2">Dining Experience</h2>
+                    <p className="text-gray-700">Learn about our elegant ambiance and top-notch service.</p>
+                    <a href="#" className="text-blue-600 hover:underline">Read More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+                {/* <h2 className='text-xl font-bold'> 1. Tell us the differences between uncontrolled and controlled components. </h2>
                 <p className='mb-5 mt-2'><FaLongArrowAltRight />
                 Controlled components refer to components that have their state and behavior controlled by the parent component. These components rely on props passed down from the parent component to update their state and behavior. Uncontrolled components refer to components that manage their own state internally. They use a ref to access the DOM element's current value and update the state accordingly.A controlled functional component is a component that receives its current value and update callback via props.
                 </p>
@@ -32,7 +65,7 @@ const Blog = () => {
                 <h2 className='text-xl  font-bold'> 4. What is a custom hook, and why will you create a custom hook? </h2>
                 <p className='mb-5 mt-2'><FaLongArrowAltRight />
                 A custom hook is a function that allows you to reuse logic across multiple components. It's a way to extract reusable logic from a component so that it can be shared between multiple components.Custom hooks are created to solve the problem of code duplication. If you find yourself writing the same code repeatedly in different components, it may be a good idea to create a custom hook that encapsulates that logic.
-                </p>
+                </p> */}
             </div>
         </div>
     );
--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -2,10 +2,21 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div  className='text-center my-20'>
-            <h1 className='text-4xl mb-6'>Contact us </h1>
-            <p className='text-xl'>Email: cheftableui@gmail.com</p>
-            <p className='text-xl'>Phone: 973-883-3488</p>
+        <div className="bg-gray-100 min-h-screen py-10">
+             <div className="container mx-auto">
+        <h1 className="text-3xl font-semibold mb-4">Contact Us</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div className="bg-white rounded-lg p-4 shadow-md">
+            <h2 className="text-xl font-semibold mb-2">Visit Us</h2>
+            <p>Address: 13/F, 32 Zhongshan No.1 Road (E)<br />Shanghai,1000<br />China</p>
+          </div>
+          <div className="bg-white rounded-lg p-4 shadow-md">
+            <h2 className="text-xl font-semibold mb-2">Contact Information</h2>
+            <p>Email: cheftableui@gmail.com<br />Phone: 973-883-3488</p>
+          </div>
+        </div>
+        
+          </div>
         </div>
     );
 };
